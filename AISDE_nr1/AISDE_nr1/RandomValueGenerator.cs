3a24a98261b609b3ee2658ad751def0904a5e661
Vonski <daniellodelapedro@interia.pl>
2015-11-08 23:42:04 +0100

    RandomValueGenerator added

---
+++ AISDE_nr1/AISDE_nr1/RandomValueGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AISDE_nr1
+{
+    class RandomValueGenerator
+    {
+        static Random seed = new Random();
+
+        double value;
+
+        public double Exp_dist(double lambda)
+        {
+            value = -Math.Log((seed.NextDouble()))/lambda;
+
+            return value;
+        }
+    }
+}
