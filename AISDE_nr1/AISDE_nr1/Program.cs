2c7aec015d20de05555c0a12dfb61c543d4b61cc
Vonski <daniellodelapedro@interia.pl>
2015-11-09 20:35:39 +0100

    Commit to merge

--- AISDE_nr1/AISDE_nr1/Program.cs
+++ AISDE_nr1/AISDE_nr1/Program.cs
@@ -11,43 +11,45 @@ namespace AISDE_nr1
     {
         static void Main(string[] args)
         {
-/*            System.Console.WriteLine("Hello world!");
+            Fifo<Packet> boom = new Fifo<Packet>();
 
-           UnorderedList<int> boom = new UnorderedList<int>();
+            boom.setSize(10);
+            for(int i = 0; i < 6; i++)
+            {
+                Packet z = new Packet();
+                z.size = i;
+                boom.Add(z);
+            }
 
-            boom.Add(2);
-            boom.Add(1);
-            boom.Add(8);
             boom.WriteOut();
-            System.Console.WriteLine("Hello world!");
-            boom.Delete();
-            boom.WriteOut();*/
+            Console.WriteLine(" ");
 
-            Heap<int> heap = new Heap<int>();
-            heap.Add(9);
-            heap.Add(8);
-            heap.Add(7);
-            heap.Add(6);
-            heap.Add(5);
-            heap.Add(4);
-            heap.Add(3);
-            heap.Add(2);
-            heap.Add(1);
-            heap.Add(0);
-            heap.WriteOut();
-            System.Console.WriteLine("\n");
-            heap.Delete();
-            heap.WriteOut();
-            System.Console.WriteLine("\n");
+            for (int i = 0; i < 3; i++)
+            {
+                boom.Delete();
+            }
 
-            heap.Delete();
-            heap.WriteOut();
-            System.Console.WriteLine("\n");
-            heap.Delete();
-            heap.WriteOut();
+            Console.WriteLine(" ");
+            boom.WriteOut();
+            Console.WriteLine(" ");
 
-            
-            System.Console.Read();
+            for (int i = 6; i < 16; i++)
+            {
+                Packet z = new Packet();
+                z.size = i;
+                boom.Add(z);
+            }
+
+            Console.WriteLine(" ");
+            boom.WriteOut();
+            Console.WriteLine(" ");
+
+            for (int i = 0; i < 11; i++)
+            {
+                boom.Delete();
+            }
+
+            Console.Read();
         }
     }
 }
