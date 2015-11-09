b33e16d9988ae1f806c7943be4de652ecbf68d5a
Vonski <daniellodelapedro@interia.pl>
2015-11-09 21:08:01 +0100

    Buffer empty

---
+++ AISDE_nr1/AISDE_nr1/Buffer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AISDE_nr1
+{
+    class Buffer
+    {
+
+    }
+}
