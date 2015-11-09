c170a7b11615c8d00d12ffe7cb1bcafbeabf0fe2
lidalida <lidka.wojciechowska1996@gmail.com>
2015-11-09 22:54:23 +0100

    Stream

--- AISDE_nr1/AISDE_nr1/Stream.cs
+++ AISDE_nr1/AISDE_nr1/Stream.cs
@@ -8,5 +8,25 @@ namespace AISDE_nr1
 {
     class Stream
     {
+        public int priority;
+        Packet packet;
+        RandomValueGenerator random_value_generator;
+
+        public Packet GeneratePacket(double lambda)
+        {
+            Packet packet = new Packet();
+            RandomValueGenerator random_value_generator = new RandomValueGenerator();
+            double size = random_value_generator.Exp_dist(lambda);
+            packet.size = (int)Math.Ceiling(size);
+            packet.priority = priority;
+            return packet;
+        }
+        public int GenerateTime(double lambda)
+        {
+            RandomValueGenerator random_value_generator = new RandomValueGenerator();
+            double t = random_value_generator.Exp_dist(lambda);
+            int time = (int)Math.Ceiling(t);
+            return time;
+        }
     }
 }
