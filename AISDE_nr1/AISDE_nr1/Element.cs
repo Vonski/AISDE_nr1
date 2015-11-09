7c408ed89e5658aab22c29bedf793edd0d988f60
lidalida <lidka.wojciechowska1996@gmail.com>
2015-11-09 12:24:01 +0100

    Program testowy dla kolejki priorytetowej

---
+++ AISDE_nr1/AISDE_nr1/Element.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AISDE_nr1
+{
+    class Element :IComparable
+    {
+        private class ComparingKeysHelper :IComparer<Element>
+        {
+            int IComparer<Element>.Compare(Element e1, Element e2)
+            {
+                if (e1.Key>e2.Key)
+                    return 1;
+                else if (e1.Key<e2.Key)
+                    return -1;
+                else
+                    return 0;
+            }
+        }
+
+        private int Key;
+
+        public void SetKey(int i)
+        {
+            Key = i;
+        }
+
+        public static IComparer<Element> ComparingKeys()
+        {
+            return (IComparer<Element>)new ComparingKeysHelper();
+        }
+
+        int IComparable.CompareTo(object obj)
+        {
+            Element e2 = (Element)obj;
+            return this.Key.CompareTo(e2.Key);
+            
+        }
+
+    }
+
+}
--- AISDE_nr1/AISDE_nr1/Program.cs
+++ AISDE_nr1/AISDE_nr1/Program.cs
@@ -1,6 +1,8 @@
 ﻿using C_sharp;
 using System;
+using System.IO;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,45 +11,107 @@ namespace AISDE_nr1
 {
     class Program
     {
+       static int M = 100, N = 100, A = 1, B = 10;
+
+        static void Test<T>(T queue)
+            where T: IPriorityQueue<Element>
+        {
+            Element element = new Element();
+            Random random = new Random();
+
+            for (int i = 0; i < A; i++)
+            {
+                element.SetKey(random.Next(1, M));
+                queue.Add(element);
+            }
+
+            for (int i=0; i<B; i++)
+            {
+                queue.Delete();
+                element.SetKey(random.Next(1, N));
+                queue.Add(element);
+            }
+
+        }
+
         static void Main(string[] args)
         {
-/*            System.Console.WriteLine("Hello world!");
+           /* int option = 1;
+            if (option == 1)
+            {
+                try
+                {
+                    using (StreamReader sr = new StreamReader("input.txt"))
+                    {
+                        M = int.Parse(sr.ReadLine());
+                        N = int.Parse(sr.ReadLine());
+                        A = int.Parse(sr.ReadLine());
+                        B = int.Parse(sr.ReadLine());
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("The file could not be read:");
+                    Console.WriteLine(e.Message);
+                }*/
 
-           UnorderedList<int> boom = new UnorderedList<int>();
+                System.IO.StreamWriter filestream_list = new System.IO.StreamWriter("timelist.txt", true);
+                System.IO.StreamWriter filestream_heap = new System.IO.StreamWriter("timeheap.txt", true);
+                Stopwatch stopwatch = new Stopwatch();
+                UnorderedList<Element> unordered_list = new UnorderedList<Element>();
+                Heap<Element> heap = new Heap<Element>();
 
-            boom.Add(2);
-            boom.Add(1);
-            boom.Add(8);
-            boom.WriteOut();
-            System.Console.WriteLine("Hello world!");
-            boom.Delete();
-            boom.WriteOut();*/
+                while(A<=500)
+                {               
+                    
+                    stopwatch.Start();
+                    Test<UnorderedList<Element>>(unordered_list);
+                    stopwatch.Stop();
+                    TimeSpan x = stopwatch.Elapsed;
 
-            Heap<int> heap = new Heap<int>();
-            heap.Add(9);
-            heap.Add(8);
-            heap.Add(7);
-            heap.Add(6);
-            heap.Add(5);
-            heap.Add(4);
-            heap.Add(3);
-            heap.Add(2);
-            heap.Add(1);
-            heap.Add(0);
-            heap.WriteOut();
-            System.Console.WriteLine("\n");
-            heap.Delete();
-            heap.WriteOut();
-            System.Console.WriteLine("\n");
 
-            heap.Delete();
-            heap.WriteOut();
-            System.Console.WriteLine("\n");
-            heap.Delete();
-            heap.WriteOut();
+                    stopwatch.Restart();
+                    Test<Heap<Element>>(heap);
+                    stopwatch.Stop();
+                    TimeSpan y = stopwatch.Elapsed;
+
+                    
+                    string timelist = x.TotalMilliseconds.ToString();
+                    string timeheap = y.TotalMilliseconds.ToString();
+
+                    filestream_list.WriteLine(timelist);
+                    filestream_heap.WriteLine(timeheap);
+                   
+                    A++;
+                }
+                filestream_list.Close();
+                filestream_heap.Close();
+                
+
+                /*string elapsedTimeList = String.Format("{0:00}min {1:00}s {2:00}ms", x.Minutes, x.Seconds, x.Milliseconds);
+                string elapsedTimeHeap = String.Format("{0:00}min {1:00}s {2:00}ms", y.Minutes, y.Seconds, y.Milliseconds);
+
+                try
+                {
+                    string path = "output.txt";
+                    using (FileStream fs = File.Create(path))
+                    {
+                        Byte[] output = new UTF8Encoding(true).GetBytes(
+                            "M=" + M + "\r\nN=" + N + "\r\nA=" + A + "\r\nB=" + B +
+                            "\r\nUnordered List: " + elapsedTimeList + "\r\nHeap: " + elapsedTimeHeap);
+                        fs.Write(output, 0, output.Length);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+        */
 
             
-            System.Console.Read();
         }
+
+        
     }
 }
